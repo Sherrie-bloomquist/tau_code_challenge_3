3b8e037a47bcb742bcdc435e078398fce169f317
Sherrie Bloomquist <sherrie.bloomquist@yahoo.com>
2016-12-12 15:49:41 -0600

    almost

--- public/scripts/cc3.js
+++ public/scripts/cc3.js
@@ -2,7 +2,59 @@ console.log( 'js' );
 
 $( document ).ready( function(){
   console.log( 'JQ' );
+// //display jokes array on DOM load
+// $('#preloadedJokes').append(jokes.length);
+
   $( '#addJokeButton' ).on( 'click', function(){
     console.log( 'addJokeButton on click');
+    postJoke();
   }); // end addJokeButton on click
+
+//post
+var postJoke = function(){
+  console.log('in postJoke');
+
+
+//assemble object to Send
+  var whoseJoke = $('#whoseJokeIn').val();
+  var jokeQuestion = $('#questionIn').val();
+  var punchLine = $('#punchlineIn').val();
+  var objectToSend = {
+    whoseJoke: whoseJoke,
+    jokeQuestion: jokeQuestion,
+    punchLine: punchLine
+  }; //end objectToSend
+console.log(objectToSend);
+
+//ajax call to server
+  $.ajax({
+    type: 'POST',
+    url: '/jokePost',
+    data: objectToSend,
+    success: function(response){
+      console.log('back from jokePost:', response);
+      outputText = '';
+      outputText += '<p><b>' + "Joke Author: " + '</b>' + response.who + '</p>';
+      outputText += '<p><b>' + "Joke: " + '</b>' + response.joke + '</p>';
+      outputText += '<p><b>' + "Punchline: " + '</b>' + response.punchline + '</p>';
+
+      $('#outputDiv').append(outputText);
+    } //end success function
+  }); //end ajax call to server
+}; //end postJoke function
+
+  //ajax call for joke array
+  // $.ajax({
+  //   type: 'GET',
+  //   url: '/',
+  //   data: jokes,
+  //   success: function(response){
+  //     $('#preloadedJokes').append(response);
+  //   }//end success function
+  //
+  //
+  //
+  // });//end ajax call for joke array
+
+
 }); // end doc ready
--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
+var port = process.env.PORT || 3000;
 // create 'urlEncodedParser' in case we want to inject it for post calls:
 var urlEncodedParser = bodyParser.urlencoded( { extended: true } );
 // use bodyParser.urlencoded throughout the app with this:
 app.use( bodyParser.urlencoded( { extended: false } ) );
 // initial jokes provided by the client
+
+
 jokes = [
   {
     whoseJoke: "Danny",
@@ -29,10 +32,15 @@ jokes = [
     punchLine: "Too many Cheetahs"
   }
 ];
+// app.post('/', function(req,res){
+// var preloadedJokes = {
+//
+//   res.send(preloadedJokes);
+// });//trying to send array to client side to post to DOM
 
 // spin up server
-app.listen( 3333, function(){
-  console.log( 'server up on 3333' );
+app.listen( port, function(){
+  console.log( 'server up on port' );
 }); // end spin up server
 
 app.get( '/', function( req, res ){
@@ -40,5 +48,25 @@ app.get( '/', function( req, res ){
   console.log( 'base url hit' );
   res.sendFile( path.resolve( 'views/index.html' ) );
 }); // end base url
+
+//jokePost
+app.post('/jokePost', function(req, res){
+  console.log('jokePost url hit req.body:', req.body);
+  var whoseJoke = req.body.whoseJoke;
+  var jokeQuestion = req.body.jokeQuestion;
+  var punchLine = req.body.punchLine;
+  // jokes.push(req.body);
+
+  console.log('var whoseJoke:', whoseJoke);
+
+var theWholeJoke = {
+  who: req.body.whoseJoke,
+  joke: req.body.jokeQuestion,
+  punchline: req.body.punchLine
+
+}; //end theWholeJoke object
+  res.send(theWholeJoke);
+
+}); //end jokePost
 
 app.use( express.static( 'public' ) );
